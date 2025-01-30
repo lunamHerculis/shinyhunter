3346c3636ba039576700a3ab139f906e2ae0501c
michaelhuber <mh@pixx.io>
2025-01-30 10:01:57 +0100

    forward-stepping for new frames. works now!

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	factor = 1
+	fastForwardSpeed = 1
 
 	displayID = 0
 
@@ -30,10 +30,15 @@ var (
 	nonShinyHex = ""
 	locationX   = 0
 	locationY   = 0
-	counter     = 0
+	saveCounter = 0
+	realCounter = 0
 )
 
 func main() {
+	fmt.Println("starting in 7...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("starting in 6...")
+	time.Sleep(1 * time.Second)
 	fmt.Println("starting in 5...")
 	time.Sleep(1 * time.Second)
 	fmt.Println("starting in 4...")
@@ -53,15 +58,40 @@ func main() {
 	time.Sleep(1 * time.Second)
 
 	for {
-		counter++
+		realCounter++
+		saveCounter++
+
 		softReset()
-		fullCycle()
-		color := getPixelColorAtPresetLocation()
-		if color == nonShinyHex {
-			fmt.Printf("no shiny, target hex: %s, color: %s, counter: %d, time: %s\n", nonShinyHex, color, counter, time.Now().Format(layoutUS))
+		toStarter()
+
+		// after 100 runs, wait and save
+		if saveCounter > 100 {
+
+			newAdditionalWaitTime := 20 * saveCounter // roughly 1.2 frames * saveCounter
+			fmt.Printf("new additional wait time in seconds: %.3f\n", float64(newAdditionalWaitTime)/1000)
+			time.Sleep(time.Duration(newAdditionalWaitTime) * time.Millisecond)
+
+			fmt.Println("saving...")
+			save()
+
+			saveCounter = 0
 			continue
 		}
-		fmt.Printf("holy shit a shiny!!!, target hex: %s, color: %s, counter: %d\n time: %s\n", nonShinyHex, color, counter, time.Now().Format(layoutUS))
+
+		newAdditionalWaitTime := 20 * saveCounter // roughly 1.2 frames * saveCounter
+		fmt.Printf("new additional wait time in seconds: %.3f\n", float64(newAdditionalWaitTime)/1000)
+		time.Sleep(time.Duration(newAdditionalWaitTime) * time.Millisecond)
+
+		takeStarter()
+		goIntoPokedex()
+
+		color := getPixelColorAtPresetLocation()
+		if color == nonShinyHex {
+			fmt.Printf("no shiny, target hex: %s, color: %s, realCounter: %d, saveCounter: %d, time: %s\n", nonShinyHex, color, realCounter, saveCounter, time.Now().Format(layoutUS))
+			continue
+		}
+
+		fmt.Printf("holy shit a shiny!!!, target hex: %s, color: %s, realCounter: %d,  saveCounter: %d\n time: %s\n", nonShinyHex, color, realCounter, saveCounter, time.Now().Format(layoutUS))
 		return
 	}
 }
@@ -84,19 +114,42 @@ func softReset() {
 	time.Sleep(10 * time.Millisecond)
 }
 
-func fullCycle() {
-	mashButton(keyA, 16*time.Second/factor)
-	mashButton(keyB, 10*time.Second/factor)
+func toStarter() {
+	mashButton(keyA, 11*time.Second/fastForwardSpeed)
+}
+
+func save() {
+	time.Sleep(1000 * time.Millisecond / fastForwardSpeed)
+	keyStroke(keyStart)
+	time.Sleep(1000 * time.Millisecond / fastForwardSpeed)
+	keyStroke(keyDown)
+	time.Sleep(500 * time.Millisecond / fastForwardSpeed)
+	keyStroke(keyDown)
+	time.Sleep(1000 * time.Millisecond / fastForwardSpeed)
+	keyStroke(keyA)
+	time.Sleep(2000 * time.Millisecond / fastForwardSpeed)
+	keyStroke(keyA)
+	time.Sleep(2000 * time.Millisecond / fastForwardSpeed)
+	keyStroke(keyA)
+	time.Sleep(9000 * time.Millisecond / fastForwardSpeed)
+}
+
+func takeStarter() {
+	mashButton(keyA, 5*time.Second/fastForwardSpeed)
+	mashButton(keyB, 10*time.Second/fastForwardSpeed)
+}
+
+func goIntoPokedex() {
 	color := getPixelColorAtPresetLocation()
 	fmt.Printf("color at pixel before pressing start: %s\n", color)
 	keyStroke(keyStart)
-	time.Sleep(1000 * time.Millisecond / factor)
+	time.Sleep(1000 * time.Millisecond / fastForwardSpeed)
 	keyStroke(keyA)
-	time.Sleep(1000 * time.Millisecond / factor)
+	time.Sleep(1000 * time.Millisecond / fastForwardSpeed)
 	keyStroke(keyA)
-	time.Sleep(700 * time.Millisecond / factor)
+	time.Sleep(700 * time.Millisecond / fastForwardSpeed)
 	keyStroke(keyA)
-	time.Sleep(3 * time.Second / factor)
+	time.Sleep(3 * time.Second / fastForwardSpeed)
 }
 
 func mashButton(key string, duration time.Duration) {
